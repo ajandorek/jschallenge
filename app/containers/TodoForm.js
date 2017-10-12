374830fe7e7b4156ebba51b4794fa01d4021c285
Alec Jandorek <ajandorek@gmail.com>
2017-10-11 22:05:36 -0500

    Post route semi-working

--- app/containers/TodoForm.js
+++ app/containers/TodoForm.js
@@ -3,23 +3,25 @@ import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
 import { createTodo } from '../actions/actions';
 import axios from 'axios';
+import request from 'superagent';
 
 import style from './App.css';
 
 class TodoForm extends Component {
     handleSubmit(e) {
-        console.log(JSON.stringify(this.refs.todo.value));
+        var data = this.refs.todo.value;
+
         e.preventDefault();
-        fetch('http://todos.stoplight.io/todos?apikey=123', {
-            method: 'POST',
-            data: {
-                name: JSON.stringify(this.refs.todo.value)
-            }
-        })
-            .then(function (response) {
-                return response.json()
-            }).then(function (body) {
-                console.log(body);
+        request
+            .post('http://todos.stoplight.io/todos?apikey=123')
+            .send({ name: data })
+            .set('Accept', 'application/json')
+            .end(function (err, res) {
+                if (err || !res.ok) {
+                    alert('Oh no! error');
+                } else {
+                    console.log('successfully posted todo');
+                }
             });
     }
     render() {
@@ -30,24 +32,6 @@ class TodoForm extends Component {
             </form>
         );
     }
-    // render() {
-    //     const { pristine, reset, submitting }
-    //     const { todo } = this.props;
-    //     return (
-    //         <div className={style.App}>
-    //             <form onSubmit={handleSubmit()}>
-    //                 <label>Add New Todo</label>
-    //                 <Field
-    //                     name="todo"
-    //                     component="input"
-    //                     type="text"
-    //                     placeholder="New Todo"
-    //                 />
-    //                 <button type="submit" disabled={ pristine || submitting }>Submit</button>
-    //             </form>
-    //         </div>
-    //     );
-    // }
 }
 
 function mapStateToProps(state) {
